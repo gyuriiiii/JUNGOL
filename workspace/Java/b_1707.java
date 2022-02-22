5afc02a16d8c805b831b60017cedfc02e26e18fa
gyuri <rbfl0616@naver.com>
2022-02-23 00:09:52 +0900

    first commit

--- workspace/Java/b_1707.java
+++ workspace/Java/b_1707.java
@@ -3,7 +3,5 @@ import java.util.Scanner;
 public class b_1707 {
     public static void main(String[] args) { 
         Scanner sc = new Scanner(System.in);
-
-        int n = sc.nextInt();
     }
 }
